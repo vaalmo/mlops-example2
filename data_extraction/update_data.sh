a31c789e9930958562610f187ac85a1a286b0f10
Valentina Morales <83479274+vaalmo@users.noreply.github.com>
2025-09-27 10:45:12 -0500

    Update Git user configuration in update_data.sh

--- data_extraction/update_data.sh
+++ data_extraction/update_data.sh
@@ -1,5 +1,5 @@
-git config --global user.name 'Edwin Montoya'
-git config --global user.email 'emontoya@eafit.edu.co'
+git config --global user.name 'vaalmo'
+git config --global user.email 'valentinamoralesvillada17@outlook.com'
 # DEBE CONFIGURAR UNA VARIABLE DE AMBIENTE GH_TOKEN EN Github-Setttings-> Developer Settings -> Personal access token - > Tokens
 # LUEGO DENTRO DEL REPOSITORIO: Settings -> Secrets and variables -> Actions -> Secrets -> Environment secrets
 #git remote set-url origin https://x-access-token:${{ secrets.GH_TOKEN }}@github.com/edwinm67/mlops-ejemplo2.git
